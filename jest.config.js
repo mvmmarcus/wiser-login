161ad3d0ee793558b0e2949a0965e5392d26df55
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-02-21 08:38:49 -0300

    feature: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,15 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/_app.tsx',
+    '!src/**/_document.tsx',
+    '!src/**/pages/index.tsx',
+    '!src/**/global.ts',
+    '!src/**/theme.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
