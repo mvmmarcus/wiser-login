be35d3f554759cf83bdbb10baaab0c98ffe69dad
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-02-23 00:35:41 -0300

    feature: add Login and Home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,39 @@ import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import GlobalStyles from 'styles/global'
+import ToastProvider from 'components/Atoms/ToastProvider'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
 
+import store from 'store'
 import theme from 'styles/theme'
+
+import 'react-toastify/dist/ReactToastify.min.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Wiser - Login</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Wiser - Login</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+        <ToastProvider />
+      </ThemeProvider>
+    </Provider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,64 @@
-import { FormSignin } from 'components/Atoms'
-import Auth from 'components/Templates/Auth'
 import { useState } from 'react'
+import Router from 'next/router'
 
-export default function Home() {
-  const [isSubmiting, setIsSubmiting] = useState(false)
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
+
+import Auth from 'components/Templates/Auth'
+import { FormSignin } from 'components/Atoms'
+import { ApplicationState } from 'store'
+import { LoginPayload } from 'store/ducks/auth/types'
+
+import * as AuthActions from 'store/ducks/auth/actions'
+
+interface StateProps {
+  token: string | null
+  error: string | null
+}
+
+interface DispatchProps {
+  loginUserAction: (payload: LoginPayload) => void
+}
+
+type Props = StateProps & DispatchProps
+
+const Login = ({ loginUserAction }: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleSubmit = (values: { email: string; password: string }) => {
-    console.log('values: ', values)
-
-    setIsSubmiting(true)
+    setIsLoading(true)
 
     setTimeout(() => {
-      setIsSubmiting(false)
+      loginUserAction(values)
+      setIsLoading(false)
     }, 1500)
   }
 
-  return (
+  const hasToken = () => {
+    const isMounted = typeof window !== 'undefined'
+    const token = isMounted ? localStorage.getItem('token') : null
+
+    if (token) {
+      return Router.push('/home')
+    }
+  }
+
+  return hasToken() ? null : (
     <Auth
       title="Olá, seja bem-vindo!"
       description="Para acessar a plataforma, faça seu login."
     >
-      <FormSignin onSubmit={handleSubmit} isSubmiting={isSubmiting} />
+      <FormSignin onSubmit={handleSubmit} isSubmiting={isLoading} />
     </Auth>
   )
 }
+
+const mapStateToProps = ({ auth }: ApplicationState) => ({
+  token: auth.token,
+  error: auth.error
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(AuthActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
