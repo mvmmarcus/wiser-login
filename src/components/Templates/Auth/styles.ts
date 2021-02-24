e12775dfc1458f913c3be2609f2bfdcfec66d076
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-02-24 01:33:14 -0300

    fixed: correct container height on small devices

--- src/components/Templates/Auth/styles.ts
+++ src/components/Templates/Auth/styles.ts
@@ -4,6 +4,7 @@ import media from 'styled-media-query'
 export const Wrapper = styled.main`
   width: 100%;
   height: 100%;
+  min-height: 600px;
   position: relative;
   padding: 32px;
   display: flex;
